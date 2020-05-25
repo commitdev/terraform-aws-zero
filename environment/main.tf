82454987614f5cffbc1d8272c7bf4c061cc35115
Bill Monkman <bmonkman@gmail.com>
2020-05-25 16:19:49 -0700

    Reorganized for the new zero project.
    Moved everything templated into a templates directory.
    Made a new readme, makefile at the root.
    Restructured the module file.

---
+++ environment/main.tf
@@ -0,0 +1,89 @@
+# Environment entrypoint
+
+locals {
+  kubernetes_cluster_name = "${var.project}-${var.environment}-${var.region}"
+}
+
+data "aws_iam_user" "ci_user" {
+  user_name = "${var.project}-ci-user"  # Should have been created in the bootstrap process
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project                 = var.project
+  environment             = var.environment
+  region                  = var.region
+  kubernetes_cluster_name = local.kubernetes_cluster_name
+}
+
+# To get the current account id
+data "aws_caller_identity" "current" {}
+
+#
+# Provision the EKS cluster
+module "eks" {
+  source = "../../modules/eks"
+
+  project              = var.project
+  environment          = var.environment
+  cluster_name         = local.kubernetes_cluster_name
+  cluster_version      = var.eks_cluster_version
+
+  iam_account_id       = data.aws_caller_identity.current.account_id
+
+  private_subnets      = module.vpc.private_subnets
+  vpc_id               = module.vpc.vpc_id
+
+  worker_instance_type = var.eks_worker_instance_type
+  worker_asg_min_size  = var.eks_worker_asg_min_size
+  worker_asg_max_size  = var.eks_worker_asg_max_size
+  worker_ami           = var.eks_worker_ami # EKS-Optimized AMI for your region: https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html
+}
+
+
+module "wildcard_domain" {
+  source = "../../modules/certificate"
+
+  region        = var.region
+  zone_name     = var.domain_name
+  domain_names  = ["*.${var.domain_name}"]
+}
+
+module "assets_domains" {
+  source = "../../modules/certificate"
+
+  region        = "us-east-1" # For CF, the cert must be in us-east-1
+  zone_name     = var.domain_name
+  domain_names  = var.s3_hosting_buckets
+}
+
+module "s3_hosting" {
+  source = "../../modules/s3_hosting"
+
+  buckets                 = var.s3_hosting_buckets
+  project                 = var.project
+  environment             = var.environment
+  certificate_arns        = module.assets_domains.certificate_arns
+  route53_zone_id         = module.assets_domains.route53_zone_id
+  certificate_validations = module.assets_domains.certificate_validations
+}
+
+module "db" {
+  source = "../../modules/database"
+
+  project                   = var.project
+  environment               = var.environment
+  vpc_id                    = module.vpc.vpc_id
+  allowed_security_group_id = module.eks.worker_security_group_id
+  instance_class            = var.db_instance_class
+  storage_gb                = var.db_storage_gb
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  environment       = var.environment
+  ecr_repositories  = var.ecr_repositories
+  ecr_principals    = [data.aws_iam_user.ci_user.arn]
+}
---
+++ environment/variables.tf
@@ -0,0 +1,59 @@
+variable "project" {
+  description = "The name of the project, mostly for tagging"
+}
+
+variable "environment" {
+  description = "The environment (dev/staging/prod)"
+}
+
+variable "region" {
+  description = "The AWS region"
+}
+
+variable "allowed_account_ids" {
+  description = "The IDs of AWS accounts for this project, to protect against mistakenly applying to the wrong env"
+  type        = list(string)
+}
+
+variable "ecr_repositories" {
+  description = "List of ECR repository names to create"
+  type        = list(string)
+}
+
+variable "eks_cluster_version" {
+  description = "EKS cluster version number to use. Incrementing this will start a cluster upgrade"
+}
+
+variable "eks_worker_instance_type" {
+  description = "Instance type for the EKS workers"
+}
+
+variable "eks_worker_asg_min_size" {
+  description = "Minimum number of instances for the EKS ASG"
+}
+
+variable "eks_worker_asg_max_size" {
+  description = "Maximum number of instances for the EKS ASG"
+}
+
+variable "eks_worker_ami" {
+  description = "The (EKS-optimized) AMI for EKS worker instances"
+}
+
+variable "s3_hosting_buckets" {
+  description = "S3 hosting buckets"
+  type = set(string)
+}
+
+variable "domain_name" {
+  description = "Domain to create a R53 Zone and ACM Cert for"
+  type = string
+}
+
+variable "db_instance_class" {
+  description = "The AWS instance class of the db"
+}
+
+variable "db_storage_gb" {
+  description = "The amount of storage to allocate for the db, in GB"
+}
