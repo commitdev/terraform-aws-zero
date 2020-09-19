bb7171dec44ac46bc7545341ff78c053bcfa7b54
Steven Shi <sshi100@hotmail.com>
2020-09-18 22:23:15 -0700

    add environment for group and set name of dependent resource

--- modules/user_access/main.tf
+++ modules/user_access/main.tf
@@ -21,18 +21,28 @@ locals {
       ]
     }
   ]
+
+  user_groups = [
+    for u in var.users : {
+      name   = u.name
+      groups = [
+        for g in aws_iam_group.access_group :
+        g.name if contains(u.roles, trimsuffix(trimprefix(g.name, "${var.project}-"), "-${var.environment}"))
+      ]
+    }
+  ]
 }
 
 # Create users and groups with polcy for AWS resource access
 resource "aws_iam_group" "access_group" {
   count = length(var.roles)
-  name  = var.roles[count.index].name
+  name  = "${var.project}-${var.roles[count.index].name}-${var.environment}"
   path  = "/users/"
 }
 
 resource "aws_iam_policy" "access_group" {
   count       = length(var.roles)
-  name        = "${var.project}-${var.roles[count.index].name}-${var.environment}"
+  name        = aws_iam_group.access_group[count.index].name
   description = "Group policy"
   policy      = var.roles[count.index].policy
 }
@@ -42,14 +52,6 @@ resource "aws_iam_group_policy_attachment" "access_group" {
   group      = aws_iam_group.access_group[count.index].name
   policy_arn = aws_iam_policy.access_group[count.index].arn
 }
-
-## Group policy
-#resource "aws_iam_group_policy" "access_group" {
-#  count  = length(var.roles)
-#  name   = "${var.project}-${var.roles[count.index].name}-${var.environment}"
-#  group  = aws_iam_group.access_group[count.index].name
-#  policy = var.roles[count.index].policy
-#}
 
 ## Users
 resource "aws_iam_user" "access_user" {
@@ -65,7 +67,7 @@ resource "aws_iam_user" "access_user" {
 resource "aws_iam_user_group_membership" "access_user_group" {
   count  = length(var.users)
   user   = aws_iam_user.access_user[count.index].name
-  groups = var.users[count.index].roles
+  groups = local.user_groups[count.index].groups
 }
 
 
@@ -95,7 +97,7 @@ data "aws_iam_policy_document" "access_assumerole_policy" {
 resource "kubernetes_cluster_role" "access_role" {
   count = length(var.roles)
   metadata {
-    name = var.roles[count.index].name
+    name = aws_iam_role.access_assumerole[count.index].name
   }
   rule {
     verbs      = ["exec", "create"]
@@ -107,15 +109,15 @@ resource "kubernetes_cluster_role" "access_role" {
 resource "kubernetes_cluster_role_binding" "access_role" {
   count = length(var.roles)
   metadata {
-    name = var.roles[count.index].name
+    name = aws_iam_role.access_assumerole[count.index].name
   }
   subject {
     kind = "Group"
-    name = var.roles[count.index].name
+    name = kubernetes_cluster_role.access_role[count.index].metadata.0.name
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = var.roles[count.index].name
+    name      = kubernetes_cluster_role.access_role[count.index].metadata.0.name
   }
 }
