724035936454a817b0713318ce8f40e429a0805d
Bill Monkman <bmonkman@gmail.com>
2020-09-23 15:14:20 -0700

    Switched away from for_each for domain validation in certificate module as it wasn't working on initial creation (#12)

--- modules/certificate/main.tf
+++ modules/certificate/main.tf
@@ -19,19 +19,20 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-# Route53 record to validate the certificate
-resource "aws_route53_record" "cert_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+locals {
+  # Change this from a set to a list so we can iterate on it.
+  # for_each was not working properly on initial creation
+  # https://github.com/terraform-providers/terraform-provider-aws/issues/14447
+  validation_records = tolist(aws_acm_certificate.cert.domain_validation_options)
+}
 
-  name            = each.value.name
-  records         = [each.value.record]
-  type            = each.value.type
+# # Route53 record to validate the certificate
+resource "aws_route53_record" "cert_validation_record" {
+  count = length(local.validation_records)
+
+  name            = local.validation_records[count.index].resource_record_name
+  records         = [local.validation_records[count.index].resource_record_value]
+  type            = local.validation_records[count.index].resource_record_type
   allow_overwrite = true
   zone_id         = data.aws_route53_zone.public.zone_id
   ttl             = 300
