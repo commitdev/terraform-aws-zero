fe8f56c62325293e97eb0dbfd3e819ccdb5b520d
Steven Shi <sshi100@hotmail.com>
2020-10-13 10:08:27 -0700

    simplify user input without name

--- modules/cloudtrail/variables.tf
+++ modules/cloudtrail/variables.tf
@@ -2,10 +2,6 @@ variable "project" {
   description = "Name of the project"
 }
 
-variable "trail_name" {
-  description = "Name of the trail"
-}
-
 variable "include_global_service_events" {
   description = "Specifies whether the trail is publishing events from global services such as IAM to the log files"
 }
