1fcdf3937d0fb140eaca1edfd18ef58cda6cf01f
Steven Shi <sshi100@hotmail.com>
2021-02-12 13:01:11 -0800

    clearn up

--- modules/cache/outputs.tf
+++ modules/cache/outputs.tf
@@ -1,19 +1,19 @@
 output "id" {
-  value       = var.cache_store == "memcached" ? module.memcached[0].cluster_id : var.cache_store == "redis" ? module.redis[0].id : "none"
+  value       = var.cache_store == "memcached" ? module.memcached[0].cluster_id : module.redis[0].id
   description = "Cache store ID"
 }
 
 output "endpoint" {
-  value       = var.cache_store == "memcached" ? module.memcached[0].cluster_address : var.cache_store == "redis" ? module.redis[0].endpoint : "none"
+  value       = var.cache_store == "memcached" ? module.memcached[0].cluster_address : module.redis[0].endpoint
   description = "Cache store address"
 }
 
 output "port" {
-  value       = var.cache_store == "memcached" ? "11211" : var.cache_store == "redis" ? module.redis[0].port : "0"
+  value       = var.cache_store == "memcached" ? "11211" : module.redis[0].port
   description = "Cache store port"
 }
 
 output "hostname" {
-  value       = var.cache_store == "memcached" ? module.memcached[0].hostname : var.cache_store == "redis" ? module.redis[0].host : "none"
+  value       = var.cache_store == "memcached" ? module.memcached[0].hostname : module.redis[0].host
   description = "Cache store hostname"
 }
