035d4a3fc6f5443f34d1d5568dd5f6c3ce90eaf6
Bill Monkman <bmonkman@gmail.com>
2021-12-07 15:16:42 -0800

    fix: more simplification of user_access code

--- modules/user_access/main.tf
+++ modules/user_access/main.tf
@@ -1,21 +1,8 @@
 # User and Roles
 
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-  users = {
-    for u in var.users : u.name => u.roles
-  }
-  roles = {
-    for r in var.roles : r.name => {
-      name       = "${var.project}-${r.name}-${var.environment}"
-      aws_policy = r.aws_policy
-    }
-  }
-}
-
 # Create group with polcy for AWS resource access
 data "aws_iam_policy_document" "access_group" {
-  for_each = local.roles
+  for_each = var.roles
 
   source_json = each.value.aws_policy
 
@@ -31,14 +18,14 @@ data "aws_iam_policy_document" "access_group" {
 }
 
 resource "aws_iam_group" "access_group" {
-  for_each = local.roles
+  for_each = var.roles
 
-  name = each.value.name
+  name = "${var.project}-${each.value.name}-${var.environment}"
   path = "/users/"
 }
 
 resource "aws_iam_policy" "access_group" {
-  for_each = local.roles
+  for_each = var.roles
 
   name        = aws_iam_group.access_group[each.key].name
   description = "Group policy"
@@ -46,16 +33,16 @@ resource "aws_iam_policy" "access_group" {
 }
 
 resource "aws_iam_group_policy_attachment" "access_group" {
-  for_each = local.roles
+  for_each = var.roles
 
   group      = aws_iam_group.access_group[each.key].name
   policy_arn = aws_iam_policy.access_group[each.key].arn
 }
 
 resource "aws_iam_user_group_membership" "access_user_group" {
-  for_each   = local.users
+  for_each   = var.users
   depends_on = [aws_iam_group.access_group]
 
-  user   = each.key
-  groups = [for r in each.value : aws_iam_group.access_group[r].name]
+  user   = each.value.name
+  groups = [for r in each.value.roles : aws_iam_group.access_group[r].name]
 }
