3e6abd53beb7772ecf58947e2ea8a7a7eaa54f56
Bill Monkman <bmonkman@gmail.com>
2021-12-09 13:48:31 -0800

    fix: syntax error in user_auth, added missing reference to k8s assumerole

--- modules/user_access/main.tf
+++ modules/user_access/main.tf
@@ -1,11 +1,19 @@
 # User and Roles
+locals {
+  # Standardize the kubernetes role name
+  k8s_role_name_format = "%s-kubernetes-%s-%s" # <project_name>-kubernetes-<role_name>-<environment>
+}
 
-# Create group with polcy for AWS resource access
+data "aws_caller_identity" "current" {}
+
+# Create group policy with added access to the corresponding k8s role
 data "aws_iam_policy_document" "access_group" {
-  for_each = var.roles
+  # Convert this with a for because terraform doesn't support for_each with list(object)
+  for_each = { for r in var.roles : r.name => r }
 
   source_json = each.value.aws_policy
 
+  # Allow anyone in this group to assume the role that will let them access the correspondingly named role in kubernetes
   statement {
     sid    = "AssumeRolePolicy"
     effect = "Allow"
@@ -13,19 +21,19 @@ data "aws_iam_policy_document" "access_group" {
       "iam:ListRoles",
       "sts:AssumeRole"
     ]
-    resources = [aws_iam_role.access_assumerole[each.key].arn]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${format(local.k8s_role_name_format, var.project, each.value.name, var.environment)}"]
   }
 }
 
 resource "aws_iam_group" "access_group" {
-  for_each = var.roles
+  for_each = { for r in var.roles : r.name => r }
 
   name = "${var.project}-${each.value.name}-${var.environment}"
   path = "/users/"
 }
 
 resource "aws_iam_policy" "access_group" {
-  for_each = var.roles
+  for_each = { for r in var.roles : r.name => r }
 
   name        = aws_iam_group.access_group[each.key].name
   description = "Group policy"
@@ -33,14 +41,14 @@ resource "aws_iam_policy" "access_group" {
 }
 
 resource "aws_iam_group_policy_attachment" "access_group" {
-  for_each = var.roles
+  for_each = { for r in var.roles : r.name => r }
 
   group      = aws_iam_group.access_group[each.key].name
   policy_arn = aws_iam_policy.access_group[each.key].arn
 }
 
 resource "aws_iam_user_group_membership" "access_user_group" {
-  for_each   = var.users
+  for_each   = { for u in var.users : u.name => u }
   depends_on = [aws_iam_group.access_group]
 
   user   = each.value.name
